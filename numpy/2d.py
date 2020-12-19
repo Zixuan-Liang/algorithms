3c72dfeab03869f08a0e20502e235c4b9a32e199
Zixuan-Liang <zixuan.liang@outlook.com>
2020-12-19 12:52:10 -0500

    Update 2d.py

--- numpy/2d.py
+++ numpy/2d.py
@@ -20,11 +20,32 @@ def concatenate(a, b, axis):
 def flip(arr, axis):
     if axis == 0:
         return arr[-1::-1]
-    elif axis == 1:
+    if axis == 1:
         return [row[-1::-1] for row in arr]
 
-def roll(arr, axis):
-    
+def roll(arr, shift, axis=None):
+    if axis is None:
+        rows, cols = len(arr), len(arr[0])
+        flat = flatten(arr)
+        shift = shift % len(flat)
+        flat = flat[-shift:] + flat[:-shift]
+        new_arr = []
+        for i in range(rows):
+            row = []
+            for j in range(cols):
+                row.append(flat[i * cols + j])
+            new_arr.append(row)
+        return new_arr
+    if axis == 0:
+        shift = shift % len(arr)
+        return arr[-shift:] + arr[:-shift]
+    if axis == 1:
+        shift = shift % len(arr[0])
+        return [row[-shift:] + row[:-shift] for row in arr]
+            
+def rot90(arr):
+    pass
+
 
 array = np.random.random_sample((2,2))
 print(array.flatten().tolist() == flatten(array))
