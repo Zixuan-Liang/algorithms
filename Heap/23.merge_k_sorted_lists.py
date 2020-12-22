570753cd2e358456250e4770fd9112aae2e2a3e2
Zixuan-Liang <zixuan.liang@outlook.com>
2020-12-21 21:36:38 -0500

    Update 23.merge_k_sorted_lists.py

--- Heap/23.merge_k_sorted_lists.py
+++ Heap/23.merge_k_sorted_lists.py
@@ -26,3 +26,33 @@ class Solution:
             if(pointers[i]):
                 heappush(minsHeap, (pointers[i].val, i))
         return ans.next
+
+# If we want to put object (not comparable) instead of index into priority queue,
+# create a wrapper class inside
+# from queue import PriorityQueue
+
+# class Solution():
+#     def mergeKLists(self, lists):
+#         """
+#         :type lists: List[ListNode]
+#         :rtype: ListNode
+#         """
+#         class Wrapper():
+#             def __init__(self, node):
+#                 self.node = node
+#             def __lt__(self, other):
+#                 return self.node.val < other.node.val
+        
+#         head = point = ListNode(0)
+#         q = PriorityQueue()
+#         for l in lists:
+#             if l:
+#                 q.put(Wrapper(l))
+#         while not q.empty():
+#             node = q.get().node
+#             point.next = node
+#             point = point.next
+#             node = node.next
+#             if node:
+#                 q.put(Wrapper(node))
+#         return head.next
