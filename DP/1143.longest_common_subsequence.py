bde3f2d769acfcb77b2d64311cc041c959bc5e1e
Zixuan-Liang <zixuan.liang@outlook.com>
2020-12-29 06:13:07 -0500

    Update 1143.longest_common_subsequence.py

--- DP/1143.longest_common_subsequence.py
+++ DP/1143.longest_common_subsequence.py
@@ -6,10 +6,11 @@ class Solution:
         dp_grid = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
         
         # Iterate up each column, starting from the last one.
-        for col in reversed(range(len(text2))):
-            for row in reversed(range(len(text1))):
+        
+        for row in reversed(range(len(text1))):
+            for col in reversed(range(len(text2))):
                 # If the corresponding characters for this cell are the same...
-                if text2[col] == text1[row]:
+                if text1[row] == text2[col]:
                     dp_grid[row][col] = 1 + dp_grid[row + 1][col + 1]
                 # Otherwise they must be different...
                 else:
@@ -18,20 +19,20 @@ class Solution:
         # The original problem's answer is in dp_grid[0][0]. Return it.
         return dp_grid[0][0]
 
-# Space optimization
+# Space optimization, with two rows instead of entire grid
 class Solution2:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         # The previous and current column starts with all 0's and like 
         # before is 1 more than the length of the first word.
-        previous = [0] * (len(text1) + 1)
-        current = [0] * (len(text1) + 1)
+        previous = [0] * (len(text2) + 1)
+        current = [0] * (len(text2) + 1)
         # Iterate up each column, starting from the last one.
-        for col in reversed(range(len(text2))):
-            for row in reversed(range(len(text1))):
-                if text2[col] == text1[row]:
-                    current[row] = 1 + previous[row + 1]
+        for row in reversed(range(len(text1))):
+            for col in reversed(range(len(text2))):
+                if text1[row] == text2[col]:
+                    current[col] = 1 + previous[col + 1]
                 else:
-                    current[row] = max(previous[row], current[row + 1])
+                    current[col] = max(previous[col], current[col + 1])
             # The current column becomes the previous one, and vice versa.
             previous, current = current, previous
         
