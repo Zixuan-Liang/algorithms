a386b4e5b89a8a218f6a88cf3d188aa21a701722
Zixuan-Liang <zixuan.liang@outlook.com>
2021-01-19 17:08:58 -0500

    Update 224.basic_calculator.py

--- Stack/224.basic_calculator.py
+++ Stack/224.basic_calculator.py
@@ -1,54 +1,21 @@
 class Solution:
 
-    def evaluate_expr(self, stack):
-
-        res = stack.pop() if stack else 0
-
-        # Evaluate the expression till we get corresponding ')'
-        while stack and stack[-1] != ')':
-            sign = stack.pop()
-            if sign == '+':
+    def calculate(self, s):
+        res, num, sign, stack = 0, 0, 1, []
+        for ss in s:
+            if ss.isdigit():
+                num = 10*num + int(ss)
+            elif ss in ["-", "+"]:
+                res += sign*num
+                num = 0
+                sign = [-1, 1][ss=="+"]
+            elif ss == "(":
+                stack.append(res)
+                stack.append(sign)
+                sign, res = 1, 0
+            elif ss == ")":
+                res += sign*num
+                res *= stack.pop()
                 res += stack.pop()
-            else:
-                res -= stack.pop()
-        return res       
-
-    def calculate(self, s: str) -> int:
-
-        stack = []
-        n, operand = 0, 0
-
-        for i in range(len(s) - 1, -1, -1):
-            ch = s[i]
-
-            if ch.isdigit():
-
-                # Forming the operand - in reverse order.
-                operand = (10**n * int(ch)) + operand
-                n += 1
-
-            elif ch != " ":
-                if n:
-                    # Save the operand on the stack
-                    # As we encounter some non-digit.
-                    stack.append(operand)
-                    n, operand = 0, 0
-
-                if ch == '(':         
-                    res = self.evaluate_expr(stack)
-                    stack.pop()        
-
-                    # Append the evaluated result to the stack.
-                    # This result could be of a sub-expression within the parenthesis.
-                    stack.append(res)
-
-                # For other non-digits just push onto the stack.
-                else:
-                    stack.append(ch)
-
-        # Push the last operand to stack, if any.
-        if n:
-            stack.append(operand)
-
-        # Evaluate any left overs in the stack.
-        return self.evaluate_expr(stack)
+                num = 0
+        return res + num*sign
