91ee7c6ffc8892e53f81fd2f6d2470385934e4f1
Zixuan-Liang <zixuan.liang@outlook.com>
2021-01-18 20:50:40 -0500

    Update 312.burst_balloons.py

--- DP/312.burst_balloons.py
+++ DP/312.burst_balloons.py
@@ -19,3 +19,23 @@ class Solution:
 
         # find the maximum number of coins obtained from adding all balloons from (0, len(nums) - 1)
         return dp(0, len(nums)-1)
+
+# bottom-up solution
+
+# class Solution:
+#     def maxCoins(self, nums: List[int]) -> int:
+
+#         # reframe problem as before
+#         nums = [1] + nums + [1]
+#         n = len(nums)
+
+#         # dp will store the results of our calls
+#         dp = [[0] * n for _ in range(n)]
+
+#         # iterate over dp and incrementally build up to dp[0][n-1]
+#         for left in range(n-2, -1, -1):
+#             for right in range(left+2, n):
+#                 # same formula to get the best score from (left, right) as before
+#                 dp[left][right] = max(nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right] for i in range(left+1, right))
+
+#         return dp[0][n-1]
