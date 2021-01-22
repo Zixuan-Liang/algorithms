3cafc065f9ebbfb3e1e6797f7f98ff264b2a9b90
Zixuan-Liang <zixuan.liang@outlook.com>
2021-01-21 23:50:18 -0500

    Update 1235.maximum_profit_in_job_scheduling.py

--- DP/1235.maximum_profit_in_job_scheduling.py
+++ DP/1235.maximum_profit_in_job_scheduling.py
@@ -1,15 +1,30 @@
 from typing import List
-import itertools
-import heapq
 
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        H = sorted(zip(startTime, itertools.repeat(1), endTime, profit))
+        length = len(startTime)
+        times = [[0, 0, 0] for _ in range(length)]
+        for i in range(length):
+            times[i][0] = startTime[i]
+            times[i][1] = endTime[i]
+            times[i][2] = profit[i]
+        times.sort() # 排序
+                
+        dp = [0 for i in range(length)]
+        
         res = 0
-        while H:
-            t = heapq.heappop(H)
-            if t[1]:
-                heapq.heappush(H, (t[2], 0, res + t[3]))
-            else:
-                res = max(res, t[2])
+        s = 0
+        pos = 0 # 标记位置
+        for i in range(length):
+            for j in range(pos, i):
+                # 区间不重合
+                if times[i][0] >= times[j][1]:
+                    # 移动 pos 的位置
+                    if j == pos:
+                        pos += 1
+                    s = max(s, dp[j])
+             
+            dp[i] = s + times[i][2]
+            res = max(res, dp[i])
+                              
         return res
