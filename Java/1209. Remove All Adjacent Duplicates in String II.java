ca5f206ef4209329f9f0e0e71942734bec7a8f50
Zixuan-Liang <zixuan.liang@outlook.com>
2021-05-19 13:08:12 -0400

    Update 1209. Remove All Adjacent Duplicates in String II.java

--- Java/1209. Remove All Adjacent Duplicates in String II.java
+++ Java/1209. Remove All Adjacent Duplicates in String II.java
@@ -1,21 +1,21 @@
-import java.util.Stack;
+import java.util.*;
 // import javafx.util.Pair;
 
 class Solution {
     public String removeDuplicates(String s, int k) {
-        Stack<Pair<Character, Integer>> stack = new Stack<Pair<Character, Integer>>();
+        ArrayDeque<Pair<Character, Integer>> stack = new ArrayDeque<Pair<Character, Integer>>();
         int count = 1;
         for(int i = 0, n = s.length() ; i < n ; i++) { 
             char c = s.charAt(i);
-            count = (stack.isEmpty() || stack.peek().getKey()!=c)? 1 : stack.peek().getValue()+1;
-            stack.push(new Pair<Character, Integer>(c, count));
-            if (stack.peek().getValue() >= k){
-                for (int j = 0; j < k; j++) stack.pop();
+            count = (stack.isEmpty() || stack.getFirst().getKey()!=c)? 1 : stack.getFirst().getValue()+1;
+            stack.addFirst(new Pair<Character, Integer>(c, count));
+            if (stack.getFirst().getValue() >= k){
+                for (int j = 0; j < k; j++) stack.removeFirst();
             }
         }
         StringBuilder res = new StringBuilder();
         while (!stack.isEmpty()){
-            res.append(stack.pop().getKey());
+            res.append(stack.removeFirst().getKey());
         }
         res.reverse();
         return res.toString();
