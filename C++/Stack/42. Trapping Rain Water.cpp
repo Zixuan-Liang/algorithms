8edfb1112527faacb03e01d784b77c726392a274
Zixuan-Liang <zixuan.liang@outlook.com>
2021-05-23 13:40:09 -0400

    42

--- C++/Stack/42. Trapping Rain Water.cpp
+++ C++/Stack/42. Trapping Rain Water.cpp
@@ -16,11 +16,13 @@ public:
                 int leftH = mono.top().first;
                 while (!mono.empty() && mono.top().first <= h) {
                     // 每pop一个，就算一段容积，（例子：4,2,0,3，算2，0，3之间的容积）
+                    // 高度以左边为准 （因为栈顶较矮）
                     res += (mono.top().first - leftH) * (i - mono.top().second - 1);
                     leftH = mono.top().first;
                     mono.pop();
                 }
                 // 如果栈非空，还要算一段容积 （例子：4,2,0,3，pop了0和2后，算4和3之间的容积）
+                // 注意此时高度以右边为准 （此时栈顶已比右边高）
                 if (!mono.empty()) res += (h - leftH) * (i - mono.top().second - 1);
                 mono.push({h, i});
             }
