05996014394e9e10a6667ef42d702b11a9c806c4
Zixuan-Liang <zixuan.liang@outlook.com>
2021-05-24 00:55:20 -0400

    52

---
+++ C++/Backtracking/52. N-Queens II.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    void backtrack(int row, set<int>& mainDiag, set<int>& minorDiag, set<int>& cols, vector<string>& board, int n, int& res) {
+        if (row == n) {res++; return ;}
+        for (int col = 0; col < n; col++) {
+            int currMainDiag = row - col;
+            int currMinorDiag = row + col;
+            if (cols.find(col)!=cols.end() || mainDiag.find(currMainDiag)!=mainDiag.end() || minorDiag.find(currMinorDiag)!=minorDiag.end()) continue;
+            cols.insert(col);
+            mainDiag.insert(currMainDiag);
+            minorDiag.insert(currMinorDiag);
+            board[row][col] = 'Q';
+            backtrack(row+1, mainDiag, minorDiag, cols, board, n, res);
+            cols.erase(col);
+            mainDiag.erase(currMainDiag);
+            minorDiag.erase(currMinorDiag);
+            board[row][col] = '.';
+        }
+    }
+    
+    int totalNQueens(int n) {
+        
+        int res = 0;
+        vector<string> board(n, string(n, '.'));
+        set<int> mainDiag, minorDiag, cols;        
+        
+        backtrack(0, mainDiag, minorDiag, cols, board, n, res);
+        
+        return res;
+    }
+};
