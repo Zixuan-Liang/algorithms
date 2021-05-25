360fb8f89295e8c0fc2573a677546ee52dad7f35
Zixuan-Liang <zixuan.liang@outlook.com>
2021-05-25 01:06:27 -0400

    62 63

--- C++/Dynamic Programming/62. Unique Paths.cpp
+++ C++/Dynamic Programming/62. Unique Paths.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     
-    int helper(int x, int y, int m, int n, map<pair<int, int>, int>& memo) {
+    int helper(int x, int y, map<pair<int, int>, int>& memo) {
         if (memo.find({x, y}) == memo.end()) {
             int res;
             if (x == 0 || y == 0) {
                 res = 1;
             }
             else {
-                res = helper(x-1, y, m, n, memo) + helper(x, y-1, m, n, memo);
+                res = helper(x-1, y, memo) + helper(x, y-1, memo);
             }
             memo[{x, y}] = res;
         }
@@ -17,6 +17,6 @@ public:
     
     int uniquePaths(int m, int n) {
         map<pair<int, int>, int> memo;
-        return helper(m-1, n-1, m, n, memo);
+        return helper(m-1, n-1, memo);
     }
 };
--- C++/Dynamic Programming/63. Unique Paths II.cpp
+++ C++/Dynamic Programming/63. Unique Paths II.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     
-    int helper(int x, int y, int m, int n, map<pair<int, int>, int>& memo, vector<vector<int>>& obstacleGrid) {
+    int helper(int x, int y, map<pair<int, int>, int>& memo, vector<vector<int>>& obstacleGrid) {
         if (memo.find({x, y}) == memo.end()) {
             int res;
             if(obstacleGrid[x][y] == 1) {
                 res = 0;
             }
             else if (x == 0) {
-                res = helper(x, y-1, m, n, memo, obstacleGrid);
+                res = helper(x, y-1, memo, obstacleGrid);
             }
             else if (y == 0) {
-                res = helper(x-1, y, m, n, memo, obstacleGrid);
+                res = helper(x-1, y, memo, obstacleGrid);
             }
             else {
-                res = helper(x-1, y, m, n, memo, obstacleGrid) + helper(x, y-1, m, n, memo, obstacleGrid);
+                res = helper(x-1, y, memo, obstacleGrid) + helper(x, y-1, memo, obstacleGrid);
             }
             memo[{x, y}] = res;
         }
@@ -26,6 +26,6 @@ public:
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
         map<pair<int, int>, int> memo;
         memo[{0, 0}] = 1;
-        return helper(m-1, n-1, m, n, memo, obstacleGrid);
+        return helper(m-1, n-1, memo, obstacleGrid);
     }
 };
