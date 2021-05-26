f6a8c7c7070d104e625a95ce3204853569788e94
Zixuan-Liang <zixuan.liang@outlook.com>
2021-05-26 19:49:28 -0400

    100

--- Legacy/Leetcode/9PalindromeNumber.py
+++
@@ -1,7 +0,0 @@
-# Determine whether an integer is a palindrome. Do this without extra space.
-def is_palindrome(x):
-    x = str(x)
-    if x == x[::-1]:
-        return True
-    else:
-        return False
