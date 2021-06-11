f7ed592889c2b19f77e829a608ec270f9b935d5d
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-10 23:15:52 -0400

    google

---
+++ C++/Breadth-First Seach/1345. Jump Game IV.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    int minJumps(vector<int>& arr) {
+        
+        int n = arr.size();
+        if (n == 1) return 0;
+        
+        unordered_map<int, vector<int>> umap;
+        for (int i = 0; i < arr.size(); i++) {
+            umap[arr[i]].push_back(i);
+        }
+        
+        queue<int> q;
+        q.push(0);
+        unordered_set<int> visited;
+        visited.insert(0);
+        int step = 0;
+        
+        while (!q.empty()) {
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+                int index = q.front();
+                q.pop();
+                
+                if (index == n - 1) return step;
+                for (int next : umap[arr[index]]) {
+                    if (visited.find(next) == visited.end()) {
+                        q.push(next);
+                        visited.insert(next);
+                    }
+                }
+                umap[arr[index]] = {};  // 避免重复搜索
+                for (int near : {index-1, index+1}) {
+                    if (near >= 0 && near < n && visited.find(near) == visited.end()) {
+                        q.push(near);
+                        visited.insert(near);
+                    }
+                }
+            }
+            step++;
+        }
+        
+        return step;
+        
+    }
+};
