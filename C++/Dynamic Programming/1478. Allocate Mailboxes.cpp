673a5ee9b4186be1dfd703c408c57d106f72e111
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-12 23:24:20 -0400

    google

--- C++/Dynamic Programming/1478. Allocate Mailboxes.cpp
+++ C++/Dynamic Programming/1478. Allocate Mailboxes.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
+    int minDistance(vector<int>& houses, int k) {
+        int n = houses.size();
+        sort(houses.begin(), houses.end());
 
-    int minDistance(vector<int>& A, int K) {
-        sort(A.begin(), A.end());
-        int n = A.size();
-        vector<int> B(n + 1, 0), dp(n, 1e6);
-        for (int i = 0; i < n; ++i)
-            B[i + 1] = B[i] + A[i];
-        for (int k = 1; k <= K; ++k)
-            for (int j = n - 1; j > k - 2; --j)
-                for (int i = k - 2; i < j; ++i) {
-                    int m1 =  (i + j + 1) / 2, m2 = (i + j + 2) / 2;
-                    int last = (B[j + 1] - B[m2]) - (B[m1 + 1] - B[i + 1]);
-                    dp[j] = min(dp[j], (i >= 0 ? dp[i] : 0) + last);
+        vector<vector<int>> medsum(n, vector<int>(n));
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                medsum[i][j] = medsum[i + 1][j - 1] + houses[j] - houses[i];
+            }
+        }
+
+        vector<vector<int>> f(n, vector<int>(k + 1, INT_MAX / 2));
+        for (int i = 0; i < n; ++i) {
+            f[i][1] = medsum[0][i];
+            for (int j = 2; j <= k && j <= i + 1; ++j) {
+                for (int i0 = 0; i0 < i; ++i0) {
+                    f[i][j] = min(f[i][j], f[i0][j - 1] + medsum[i0 + 1][i]);
                 }
-        return dp[n - 1];
+            }
+        }
+
+        return f[n - 1][k];
     }
-    
 };
