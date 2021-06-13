90f7a95e989936ea5ef8a279a23c698ec0c97a99
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-13 18:11:41 -0400

    google

---
+++ C++/Heap/1834. Single-Threaded CPU.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        vector<int> res, idx(tasks.size());
+        priority_queue<pair<int, int>> pq;
+        for (int i = 0; i < idx.size(); i++) idx[i] = i;
+        sort(begin(idx), end(idx), [&](int i, int j) { return tasks[i][0] < tasks[j][0]; });
+        for (long i = 0, time = 1; i < idx.size() || !pq.empty();) {
+            for (; i < idx.size() && tasks[idx[i]][0] <= time; ++i)
+                pq.push({-tasks[idx[i]][1], -idx[i]});
+            if (!pq.empty()) {
+                auto [procTime, j] = pq.top(); pq.pop();
+                time -= procTime;
+                res.push_back(-j);
+            }
+            else
+                time = tasks[idx[i]][0];
+        }
+        return res;
+    }
+    
+};
