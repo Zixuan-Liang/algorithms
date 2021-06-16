40681ab321709430f9a966954fd877a17798e345
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-16 19:08:20 -0400

    google

--- C++/Sort/1094. Car Pooling.cpp
+++ C++/Sort/1094. Car Pooling.cpp
@@ -1,15 +1,34 @@
+// class Solution {
+// public:
+//     bool carPooling(vector<vector<int>>& trips, int capacity) {
+//         vector<vector<int>> vec;
+//         for (auto& trip : trips) {
+//             vec.push_back({trip[1], 1, trip[0]});
+//             vec.push_back({trip[2], -1, trip[0]});
+//         }
+//         sort(vec.begin(), vec.end());
+//         int passengers = 0;
+//         for (auto& v : vec) {
+//             passengers += v[1] * v[2];
+//             if (passengers > capacity) return false;
+//         }
+//         return true;
+//     }
+// };
+
+#include <numeric>
+
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<vector<int>> vec;
+        vector<vector<int>> buckets(1001);
         for (auto& trip : trips) {
-            vec.push_back({trip[1], 1, trip[0]});
-            vec.push_back({trip[2], -1, trip[0]});
+            buckets[trip[1]].push_back(trip[0]);
+            buckets[trip[2]].push_back(-trip[0]);
         }
-        sort(vec.begin(), vec.end());
         int passengers = 0;
-        for (auto& v : vec) {
-            passengers += v[1] * v[2];
+        for (auto& bucket : buckets) {
+            passengers += accumulate(bucket.begin(), bucket.end(), 0);
             if (passengers > capacity) return false;
         }
         return true;
