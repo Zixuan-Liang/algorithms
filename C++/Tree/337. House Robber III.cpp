911aa00492a2f920b88c79b52bde19abf9588b41
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-18 01:59:24 -0400

    google

---
+++ C++/Tree/337. House Robber III.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> helper(TreeNode* root) {
+        if (!root) {
+            return {0, 0};
+        }
+        vector<int> L = helper(root->left);
+        vector<int> R = helper(root->right);
+        int d1 = root->val + L[1] + R[1];
+        int d2 = max(L[0], L[1]) + max(R[0], R[1]);
+        return {d1, d2};
+    }
+    
+    int rob(TreeNode* root) {
+        vector<int> res = helper(root);
+        return max(res[0], res[1]);
+    }
+};
