051dcc4b29755c9a6aad76c425fa9ac3f870537e
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-19 00:51:53 -0400

    google

--- C++/Stack/227. Basic Calculator II.cpp
+++ C++/Stack/227. Basic Calculator II.cpp
@@ -28,3 +28,35 @@ public:
         return accumulate(stack.begin(), stack.end(), 0);
     }
 };
+
+// Not using stack, O(n) time O(1) space, remember lastNum and currNum and combine them
+// if * or / comes up.
+// class Solution {
+// public:
+//     int calculate(string s) {
+//         int length = s.length();
+//         if (length == 0) return 0;
+//         int currentNumber = 0, lastNumber = 0, result = 0;
+//         char sign = '+';
+//         for (int i = 0; i < length; i++) {
+//             char currentChar = s[i];
+//             if (isdigit(currentChar)) {
+//                 currentNumber = (currentNumber * 10) + (currentChar - '0');
+//             }
+//             if (!isdigit(currentChar) && !iswspace(currentChar) || i == length - 1) {
+//                 if (sign == '+' || sign == '-') {
+//                     result += lastNumber;
+//                     lastNumber = (sign == '+') ? currentNumber : -currentNumber;
+//                 } else if (sign == '*') {
+//                     lastNumber = lastNumber * currentNumber;
+//                 } else if (sign == '/') {
+//                     lastNumber = lastNumber / currentNumber;
+//                 }
+//                 sign = currentChar;
+//                 currentNumber = 0;
+//             }
+//         }
+//         result += lastNumber;
+//         return result;  
+//     }
+// };
