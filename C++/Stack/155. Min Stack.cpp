dca001c4f4b5760754b65c523049aedbc11cef40
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-19 19:24:52 -0400

    155

---
+++ C++/Stack/155. Min Stack.cpp
@@ -0,0 +1,42 @@
+class MinStack {
+    
+    stack<int> stk;
+    stack<int> minStack;
+    
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stk.push(val);
+        if (minStack.empty() || val <= minStack.top()) {
+            minStack.push(val);
+        }
+    }
+    
+    void pop() {
+        if (minStack.top() == stk.top()) {
+            minStack.pop();
+        }
+        stk.pop();
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
