f63ec49883c52db158e9a6ea1bd43ff79b6f6b16
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-20 21:59:08 -0400

    google

---
+++ C++/Dynamic Programming/727. Minimum Window Subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string minWindow(string S, string T) {
+        int m = S.size(), n = T.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        for (int i = 0; i < m; i++) 
+            if (S[i] == T[0]) dp[0][i] = i;
+        for (int j = 1; j < n; j++) {
+            int k = -1;
+            for (int i = 0; i < m; i++) {
+                if (k != -1 && S[i] == T[j]) dp[j][i] = k;
+                if (dp[j-1][i] != -1) k = dp[j-1][i];
+            }
+        }
+        int st = -1, len = INT_MAX;
+        for (int i = 0; i < m; i++) {
+            if (dp[n-1][i] != -1 && i-dp[n-1][i]+1 < len) {
+                st = dp[n-1][i];
+                len = i-dp[n-1][i]+1;
+            }    
+        }
+        return st == -1? "":S.substr(st, len);
+    }
+};
