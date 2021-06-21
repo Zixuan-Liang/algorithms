f04a00c50ff5f36bbe53a46c4c6a0b81bbe00b5f
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-21 13:30:15 -0400

    google

--- C++/Stack/85. Maximal Rectangle.cpp
+++ C++/Stack/85. Maximal Rectangle.cpp
@@ -33,23 +33,26 @@ public:
         if (m == 0) return 0;
         int n = matrix[0].size();
         
-        vector<vector<int>> intMatrix(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                intMatrix[i][j] = matrix[i][j] - '0';
-            }
-        }
+        // vector<vector<int>> intMatrix(m, vector<int>(n, 0));
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         intMatrix[i][j] = matrix[i][j] - '0';
+        //     }
+        // }
+        vector<int> dp(n, 0);
         
         int res = 0;
         for (int i = 0; i < m; i++) {
-            if (i > 0) {
+            // if (i > 0) {
                 for (int j = 0; j < n; j++) {
-                    if (intMatrix[i-1][j] != 0 && intMatrix[i][j] == 1) {
-                        intMatrix[i][j] = 1 + intMatrix[i-1][j];
-                    }
+                    // if (intMatrix[i-1][j] != 0 && intMatrix[i][j] == 1) {
+                    //     intMatrix[i][j] = 1 + intMatrix[i-1][j];
+                    // }
+                    dp[j] = matrix[i][j] == '1' ? dp[j]+1 : 0;
                 }
-            }
-            res = max(res, largestRectangleArea(intMatrix[i]));
+            // }
+            // res = max(res, largestRectangleArea(intMatrix[i]));
+            res = max(res, largestRectangleArea(dp));
         }
         return res;
     }
