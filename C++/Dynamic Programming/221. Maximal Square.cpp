d6377aecda795366adbdd41dfed88baf097853d3
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-23 03:10:10 -0400

    google

---
+++ C++/Dynamic Programming/221. Maximal Square.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        int res = 0;
+        for (int i = 1; i < m+1; i++) {
+            for (int j = 1; j < n+1; j++) {
+                if (matrix[i-1][j-1] == '1') {
+                    dp[i][j] = 1 + min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
+                    res = max(res, dp[i][j] * dp[i][j]);
+                }
+            }
+        }
+        return res;
+    }
+};
