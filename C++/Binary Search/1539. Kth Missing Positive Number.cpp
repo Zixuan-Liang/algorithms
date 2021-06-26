7fdc10f51c29c61a68918b099c3b6e50326f3fc9
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-26 01:41:09 -0400

    google

--- C++/Binary Search/1539. Kth Missing Positive Number.cpp
+++ C++/Binary Search/1539. Kth Missing Positive Number.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int left = 0, right = arr.size() - 1;
-        while (left <= right) {
-            int pivot = (left + right) / 2;
-            if (arr[pivot] - pivot - 1 < k) {
-                left = pivot + 1;
-            }
-            else {
-                right = pivot - 1;
+        int low = 0, high = arr.size();
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] - mid - 1 >= k) {
+                high = mid;
+            } else {
+                low = mid + 1;
             }
         }
-        return left + k;
+        return low + k;
     }
 };
