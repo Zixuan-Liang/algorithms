4771b699d4e77fe3bdf00acb188c4c64f7cbd461
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-27 14:19:02 -0400

    google

--- C++/Tree/Balanced Tree (Ordered Set)/731. My Calendar II.cpp
+++ C++/Tree/Balanced Tree (Ordered Set)/731. My Calendar II.cpp
@@ -1,39 +1,28 @@
 class MyCalendarTwo {
-    map<int, int> treeMap1, treeMap2;
+private:
+    map<int, int> m;
 public:
-    bool book(int start, int end) {
-        auto it = treeMap1.upper_bound(start);
-        if (it == treeMap1.end() || it->second >= end) {
-            treeMap1[end] = start;
-            return true;
-        }
-        vector<int> toRemove;
-        vector<pair<int, int>> toAdd;
-        for (; it != treeMap1.end() && it->second < end; it++) {
-            int s2 = max(start, it->second);
-            int e2 = min(end, it->first);
-            if (start > it->second) {
-                start = it->second;
-            }
-            if (end < it->first) {
-                end = it->first;
-            }
-            
-            auto it2 = treeMap2.upper_bound(s2);
-            if (it2 != treeMap2.end() && it2->second < e2) {
-                return false;
-            }
-            
-            toRemove.push_back(it->first);
-            toAdd.emplace_back(e2, s2);
-        }
-        for (int e : toRemove) {
-            treeMap1.erase(e);
-        }
-        treeMap1[end] = start;
-        for (const pair<int, int> &p : toAdd) {
-            treeMap2[p.first] = p.second;
+    
+    //Parameter n sets the max number of bookings allowed to overlap
+    //if n is set to 1 if will check for double booking
+    //if n is set to 2 if will check for triple booking etc
+    bool canAdd(int n){
+        int count = 0;
+        for (auto entry: m){
+            count+=entry.second;
+            if(count>n) return false;
         }
         return true;
+    }
+    
+    bool book(int start, int end) {
+        m[start]++;
+        m[end]--;
+        if(canAdd(2)) return true;//Check for triple booking (n=2)
+        else{//restore the original counts since we are not booking this room
+            m[start]--;
+            m[end]++;
+            return false;
+        }
     }
 };
