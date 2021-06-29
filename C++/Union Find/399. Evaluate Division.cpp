3873e71986b8a9eecd6c7220db8da0f1ff5c09ae
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-29 01:25:23 -0400

    google

--- C++/Union Find/399. Evaluate Division.cpp
+++ C++/Union Find/399. Evaluate Division.cpp
@@ -1,58 +1,44 @@
 class Solution {
 public:
+    
+    unordered_map<string, string> parents;
+    unordered_map<string, double> vals;
+    
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        unordered_map<string, Node*> map;
-        vector<double> res;
-        for (int i = 0; i < equations.size(); i ++) {
-            string s1 = equations[i][0], s2 = equations[i][1];
-            if (map.count(s1) == 0 && map.count(s2) == 0) {
-                map[s1] = new Node();
-                map[s2] = new Node();
-                map[s1] -> value = values[i];
-                map[s2] -> value = 1;
-                map[s1] -> parent = map[s2];
-            } else if (map.count(s1) == 0) {
-                map[s1] = new Node();
-                map[s1] -> value = map[s2] -> value * values[i];
-                map[s1] -> parent = map[s2];
-            } else if (map.count(s2) == 0) {
-                map[s2] = new Node();
-                map[s2] -> value = map[s1] -> value / values[i];
-                map[s2] -> parent = map[s1];
-            } else {
-                unionNodes(map[s1], map[s2], values[i], map);
-            }
+        vector<double> res(queries.size());
+        for (int i = 0; i < values.size(); i++) {
+            uni(equations[i][0], equations[i][1], values[i]);
         }
-
-        for (auto query : queries) {
-            if (!map.count(query[0]) || !map.count(query[1]) || findParent(map[query[0]]) != findParent(map[query[1]]))
-                res.push_back(-1);
-            else
-                res.push_back(map[query[0]] -> value / map[query[1]] -> value);
+        for (int i = 0; i < queries.size(); i++) {
+            string x = queries[i][0], y = queries[i][1];
+            res[i] = (parents.count(x) && parents.count(y) && find(x)==find(y)) ? vals[x] / vals[y] : -1.0;
         }
         return res;
     }
     
-private:
-    struct Node {
-        Node* parent;
-        double value = 0.0;
-        Node()  {parent = this;}
-    };
-    
-    void unionNodes(Node* node1, Node* node2, double num, unordered_map<string, Node*>& map) {
-        Node* parent1 = findParent(node1), *parent2 = findParent(node2);
-        double ratio = node2 -> value * num / node1 -> value;
-        for (auto it = map.begin(); it != map.end(); it ++)
-            if (findParent(it -> second) == parent1)
-                it -> second -> value *= ratio;
-        parent1 -> parent = parent2;
+    void add(string x) {
+        if (parents.count(x)) {
+            return ;
+        }
+        parents[x] = x;
+        vals[x] = 1.0;
     }
     
-    Node* findParent(Node* node) {
-        if (node -> parent == node)
-            return node;
-        node -> parent = findParent(node -> parent);
-        return node -> parent;
+    string find(string x) {
+        string p = parents[x];
+        if (x != p) {
+            string pp = find(p);
+            vals[x] = vals[x] * vals[p];
+            parents[x] = pp;
+        }
+        return parents[x];
+    }
+    
+    void uni(string x, string y, double v) {
+        add(x);
+        add(y);
+        string px = find(x), py = find(y);
+        parents[px] = py;
+        vals[px] = v * vals[y] / vals[x];
     }
 };
