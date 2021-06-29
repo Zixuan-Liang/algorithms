a861208ce0b58dc4c82b39082c954939606fe239
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-29 17:41:12 -0400

    google

--- C++/Tree/549. Binary Tree Longest Consecutive Sequence II.cpp
+++ C++/Tree/549. Binary Tree Longest Consecutive Sequence II.cpp
@@ -12,43 +12,35 @@
 class Solution {
 public:
     
-    vector<int> helper(TreeNode* root, int& res) {
-        int inc, dec;
-        if (!root) {
-            inc = 0;
-            dec = 0;
+    int ans = 0;
+    
+    pair<int, int> helper(TreeNode* root) {
+        if (!root) return {0, 0};
+        int inc = 1, dec = 1;
+        if (root->left) {
+            auto [lInc, lDec] = helper(root->left);
+            if (root->val == root->left->val - 1) {
+                inc = max(inc, lInc + 1);
+            }
+            else if (root->val == root->left->val + 1) {
+                dec = max(dec, lDec + 1);
+            }
         }
-        else if (!root->left && !root->right) {
-            inc = 1;
-            dec = 1;
-        }
-        else {
-            vector<int> left = helper(root->left, res), right = helper(root->right, res);
-            
-            bool lInc = root->left && root->val == root->left->val - 1;
-            bool rInc = root->right && root->val == root->right->val - 1;
-            bool lDec = root->left && root->val == root->left->val + 1;
-            bool rDec = root->right && root->val == root->right->val + 1;
-            
-            int lIncLen = (lInc) ? left[0]+1 : 1;
-            int rIncLen = (rInc) ? right[0]+1 : 1;
-            
-            int lDecLen = (lDec) ? left[1]+1 : 1;
-            int rDecLen = (rDec) ? right[1]+1 : 1;
-            
-            inc = max(lIncLen, rIncLen);
-            dec = max(lDecLen, rDecLen);
-            
-            if (lInc && rDec) res = max(res, left[0]+right[1]+1);
-            if (lDec && rInc) res = max(res, left[1]+right[0]+1);
-        }
-        res = max({res, inc, dec});
+        if (root->right) {
+            auto [rInc, rDec] = helper(root->right);
+            if (root->val == root->right->val - 1) {
+                inc = max(inc, rInc + 1);
+            }
+            else if (root->val == root->right->val + 1) {
+                dec = max(dec, rDec + 1);
+            }
+        }        
+        ans = max(ans, inc + dec - 1);
         return {inc, dec};
     }
     
     int longestConsecutive(TreeNode* root) {
-        int res = 0;
-        helper(root, res);
-        return res;
+        helper(root);
+        return ans;
     }
 };
