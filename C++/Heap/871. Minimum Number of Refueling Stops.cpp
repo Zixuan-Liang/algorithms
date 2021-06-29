215781568a7daac5dba6111c720ab99b465c2d00
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-28 20:17:58 -0400

    google

--- C++/Heap/871. Minimum Number of Refueling Stops.cpp
+++ C++/Heap/871. Minimum Number of Refueling Stops.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
         priority_queue<int> pq;
-        stations.push_back({target, INT_MAX});
+        stations.push_back({target, 0});
         int res = 0, prev = 0;
         for (auto& station : stations) {
             int location = station[0], capacity = station[1];
