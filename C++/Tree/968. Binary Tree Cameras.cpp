fd92fd11b4a06bf7707041875677eb6d32379728
Zixuan-Liang <zixuan.liang@outlook.com>
2021-06-29 20:22:21 -0400

    google

--- C++/Tree/968. Binary Tree Cameras.cpp
+++ C++/Tree/968. Binary Tree Cameras.cpp
@@ -14,18 +14,20 @@ public:
     
     vector<int> helper(TreeNode* root) {
         if (!root) {
-            return {0, 0, 999999};
+            return {0, 0, 1};
         }
-        // 0: Strict ST; All nodes below this are covered, but not this one
-        // 1: Normal ST; All nodes below and incl this are covered - no camera
+        // 0: All nodes below this are covered, but not this one
+        // 1: All nodes below and incl this are covered - no camera
         // 2: Placed camera; All nodes below this are covered, plus camera here
         vector<int> L = helper(root->left);
         vector<int> R = helper(root->right);
-        int mL12 = min(L[1], L[2]);
-        int mR12 = min(R[1], R[2]);
+        
         int d0 = L[1] + R[1];
-        int d1 = min(L[2] + mR12, R[2] + mL12);
-        int d2 = 1 + min(L[0], mL12) + min(R[0], mR12);
+        
+        int d1 = min(L[2] + min(R[1], R[2]), R[2] + min(L[1], L[2]));
+        
+        int d2 = 1 + min({L[0], L[1], L[2]}) + min({R[0], R[1], R[2]});
+        
         return {d0, d1, d2};
     }
     
