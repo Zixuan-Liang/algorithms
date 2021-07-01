69dd184f025ad418e59559bee6ec07d0b8b556ad
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-01 02:28:49 -0400

    google

--- C++/Sort/853. Car Fleet.cpp
+++ C++/Sort/853. Car Fleet.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    
-    static bool sortByPos(pair<int, int>& x, pair<int, int>& y) {
-        return x.second > y.second;
-    }
-    
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        vector<pair<int, int>> speedPos;
-        for (int i = 0; i < position.size(); i++) {
-            speedPos.push_back({speed[i], position[i]});
+        vector<vector<int>> cars;
+        int n = position.size();
+        for (int i = 0; i < n; i++) {
+            cars.push_back({position[i], speed[i]});
         }
-        sort(speedPos.begin(), speedPos.end(), sortByPos);
-        vector<double> timeToDest;
-        for (auto& s : speedPos) {
-            timeToDest.push_back( (target - s.second) / (double)s.first );
-        }
-        int fleet = 0;
-        int index = 0;
-        double prevTime = -1;
-        while (index < timeToDest.size() && timeToDest[index] > prevTime) {
-            fleet++;
-            prevTime = timeToDest[index];
-            while (index < timeToDest.size() && timeToDest[index] <= prevTime) {
-                index++;
+        sort(cars.begin(), cars.end());
+        int ans = 0;
+        double frontFleetT2D = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            double thisCarT2D = 1.0 * (target - cars[i][0]) / cars[i][1];
+            if (thisCarT2D > frontFleetT2D) {
+                ans++;
+                frontFleetT2D = thisCarT2D;
             }
         }
-        return fleet;
+        return ans;
     }
 };
