33ace1690b7559f7f610f6ba65ed68a42d823f74
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-01 18:04:42 -0400

    google

--- C++/Stack/71. Simplify Path.cpp
+++ C++/Stack/71. Simplify Path.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
+    
+    vector<string> split(string s, string del = " ") {
+        vector<string> ans;
+        int start = 0;
+        int end = s.find(del, start);
+        while (end != -1) {
+            ans.push_back(s.substr(start, end - start));
+            start = end + del.size();
+            end = s.find(del, start);
+        }
+        ans.push_back(s.substr(start, end - start));
+        return ans;
+    }
+    
     string simplifyPath(string path) {
-        deque<string> stk;
-        string currStr = "";
-        int index = 0;
-        while (index < path.size()) {
-            if (path[index] == '/') {
-                if (currStr != "") stk.push_back(currStr);
-                currStr = "";
-                index++;
+        vector<string> s = split(path, "/");
+        deque<string> dq;
+        for (string& dir_file : s) {
+            if (dir_file == "..") {
+                if (!dq.empty()) {
+                    dq.pop_back();
+                }
             }
-            else if (path[index] == '.'){
-                // pattern: /../
-                if (path.substr(index-1, 3) == "/.." && (index+2 >= path.size() || path[index+2] == '/')) {
-                    if (!stk.empty()) stk.pop_back();
-                    currStr = "";
-                    index++;
-                }
-                // pattern /./
-                else if (path.substr(index-1, 2) == "/." && (index+1 >= path.size() || path[index+1] == '/')) {
-                    currStr = "";
-                }
-                // otherwise treated as directory/file name
-                else {
-                    currStr += '.';
-                }
-                index++;
+            else if (dir_file == "." or dir_file == "") {
+                continue;
             }
             else {
-                while (index < path.size() && path[index] != '/') {
-                    currStr += path[index];
-                    index++;
-                }
+                dq.push_back(dir_file);
             }
         }
-        if (currStr!="") stk.push_back(currStr);
-        if (stk.empty()) return "/";
-        string res = "";
-        while (!stk.empty()) {
-            res += "/" + stk.front();
-            stk.pop_front();
+        if (dq.empty()) return "/";  // 注意corner case：空栈直接返回"/"
+        string ans = "";
+        while (!dq.empty()) {
+            ans += "/" + dq.front();
+            dq.pop_front();
         }
-        return res;
+        return ans;
     }
 };
+
+
