bab43725bee2fea05d90c827a4b6fb84682a0734
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-02 17:20:41 -0400

    google

--- C++/Tree/Segment Tree/493. Reverse Pairs.cpp
+++ C++/Tree/Segment Tree/493. Reverse Pairs.cpp
@@ -1,43 +1,72 @@
+struct SegmentTree {
+    
+    SegmentTree* left;
+    SegmentTree* right;
+    int count;
+    long min;
+    long max;
+    
+    // nums is sorted
+    SegmentTree(vector<int>& nums, int l, int r): left(nullptr), right(nullptr) {
+        min = nums[l];
+        max = nums[r];
+        count = 0;
+        if (l == r) return;
+        int m = l + (r - l) / 2;
+        if (l <= m) {
+            left = new SegmentTree(nums, l, m);
+        }
+        if (m + 1 <= r) {
+            right = new SegmentTree(nums, m + 1, r);
+        }
+    }
+    
+    void updateTree(int value) {
+        if (value < min or value > max) {
+            return ;
+        }
+        if (left and left->min <= value and value <= left->max) {
+            left->updateTree(value);
+        }
+        else if (right and right->min <= value and value <= right->max) {
+            right->updateTree(value);
+        }
+        count++;
+    }
+    
+    int queryTree(long minValue, long maxValue) {
+        if (minValue<=min && max<=maxValue) {
+            return count;
+        }
+        else if (maxValue<min || minValue>max) {
+            return 0;
+        }
+        else {
+            int q1 = left ? left->queryTree(minValue, maxValue) : 0;
+            int q2 = right ? right->queryTree(minValue, maxValue) : 0;
+            return q1 + q2;
+        }
+    }
+};
+
 class Solution {
 public:
+    
     int reversePairs(vector<int>& nums) {
-        set<int> s;
-        for (int& x : nums) s.insert(x);
-        vector<long long> v;
-        for (auto it = s.begin(); it != s.end(); it++) v.push_back(*it);
-        unordered_map<int, int> m;
-        for (int i = 0; i < v.size(); i++) m[v[i]] = i;
-        int n = v.size(), ret = 0;
-        vector<int> segtree(2 * n);
-        for (int i = nums.size()-1; i >= 0; i--)
-        {
-            if (v[0] * 2 < nums[i])
-            {
-                int left = 0, right = n-1, mid = (right + 1) / 2;
-                while (left < right)
-                {
-                    if (v[mid] * 2 < nums[i]) left = mid;
-                    else right = mid - 1;
-                    mid = (left + right + 1) / 2;
-                }
-                ret += query(segtree, 0, mid, n);
-            }
-            update(segtree, m[nums[i]], n);
+        
+        unordered_set<int> uset;
+        for (int n : nums) uset.insert(n);
+        vector<int> v(uset.begin(), uset.end());
+        sort(v.begin(), v.end());
+        
+        SegmentTree* tree = new SegmentTree(v, 0, v.size() - 1);
+        
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            ans += tree->queryTree(2 * (long)nums[i] + 1, (long)v.back());
+            tree->updateTree(nums[i]);
         }
-        return ret;
+        return ans;
     }
-    int query(vector<int>& segtree, int a, int b, int n)
-    {
-        int ret = 0;
-        for (a += n, b += n; a <= b; a >>= 1, b >>= 1)
-        {
-            if (a & 1) ret += segtree[a++];
-            if (!(b & 1)) ret += segtree[b--];
-        }
-        return ret;
-    }
-    void update(vector<int>& segtree, int a, int n)
-    {
-        for (a += n; a; a >>= 1) segtree[a]++;
-    }
+
 };
