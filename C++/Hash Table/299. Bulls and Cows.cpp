c3eab85c574a637d8a44f9361f0dd682fef330f9
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-02 23:37:11 -0400

    google

--- C++/Hash Table/299. Bulls and Cows.cpp
+++ C++/Hash Table/299. Bulls and Cows.cpp
@@ -8,17 +8,44 @@ public:
         int bull = 0, cow = 0;
         for (int i = 0; i < guess.size(); i++) {
             char c = guess[i];
-            if (umap.find(c) != umap.end()) {
-                if (c == secret[i]) {
-                    bull++;
-                    cow -= (int)umap[c]<=0;
-                }
-                else {
-                    cow += (int)umap[c]>0;
-                }
-                umap[c]--;
+            if (c == secret[i]) {
+                bull++;
+                if (--umap[c] == 0) umap.erase(c);
+            }
+        }
+        for (int i = 0; i < guess.size(); i++) {
+            char c = guess[i];
+            if (c != secret[i] && umap.count(c)) {
+                cow++;
+                if (--umap[c] == 0) umap.erase(c);
             }
         }
         return to_string(bull) + 'A' + to_string(cow) + 'B';
+    }
+};
+
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int n = secret.size();
+        
+        map<char, int> present;
+        int b = 0, c = 0;
+        for(int i=0; i<n; i++){
+            if(secret[i] == guess[i])
+                b++;
+            else
+                present[secret[i]]++;
+        }
+        
+        for(int i=0; i<n; i++){
+            if(guess[i] != secret[i]){
+                if(present[guess[i]] > 0){
+                    c++;
+                    present[guess[i]]--;
+                }
+            }
+        }
+        return to_string(b) + "A" + to_string(c) + "B";
     }
 };
