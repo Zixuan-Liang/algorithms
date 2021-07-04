a3587c4b95b5dc6b87d48421fd1ccf50108854d4
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-03 23:09:04 -0400

    google

---
+++ C++/Sliding Window/1871. Jump Game VII.cpp
@@ -0,0 +1,27 @@
+// Explanation
+// dp[i] = true if we can reach s[i].
+// pre means the number of previous position that we can jump from.
+
+
+// Complexity
+// Time O(n)
+// Space O(n)
+
+class Solution {
+public:
+
+    bool canReach(string s, int minJ, int maxJ) {
+        int n = s.size(), pre = 0;
+        vector<bool> dp(n, false);
+        dp[0] = true;
+        for (int i = 1; i < n; ++i) {
+            if (i >= minJ)
+                pre += dp[i - minJ]; // sliding window
+            if (i > maxJ)
+                pre -= dp[i - maxJ - 1]; // sliding window
+            dp[i] = pre > 0 && s[i] == '0';
+        }
+        return dp[n - 1];
+    }
+    
+};
