19a89fcffbd0352a809f2ddbd95c64124f000a2a
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-03 20:45:21 -0400

    google

--- C++/Dynamic Programming/727. Minimum Window Subsequence.cpp
+++ C++/Dynamic Programming/727. Minimum Window Subsequence.cpp
@@ -1,24 +1,40 @@
+// dp[i][j] stores the starting index of the substring where T has length i and S has length j.
+
+// So dp[i][j] would be:
+// if T[i - 1] == S[j - 1], this means we could borrow the start index from dp[i - 1][j - 1] to make the current substring valid;
+// else, we only need to borrow the start index from dp[i][j - 1] which could either exist or not.
+
+// Finally, go through the last row to find the substring with min length and appears first.
+
+
 class Solution {
 public:
-    string minWindow(string S, string T) {
-        int m = S.size(), n = T.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        for (int i = 0; i < m; i++) 
-            if (S[i] == T[0]) dp[0][i] = i;
-        for (int j = 1; j < n; j++) {
-            int k = -1;
-            for (int i = 0; i < m; i++) {
-                if (k != -1 && S[i] == T[j]) dp[j][i] = k;
-                if (dp[j-1][i] != -1) k = dp[j-1][i];
+    string minWindow(string source, string target) {
+        int m = target.size(), n = source.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j + 1;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (target[i - 1] == source[j - 1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j] = dp[i][j-1];
+                }
             }
         }
-        int st = -1, len = INT_MAX;
-        for (int i = 0; i < m; i++) {
-            if (dp[n-1][i] != -1 && i-dp[n-1][i]+1 < len) {
-                st = dp[n-1][i];
-                len = i-dp[n-1][i]+1;
-            }    
+        // Finally, go through the last row to find the substring with min length and appears first.
+        int start = 0, len = n + 1;
+        for (int j = 1; j <= n; j++) {
+            if (dp[m][j] != 0) {
+                if (j - dp[m][j] + 1 < len) {
+                    start = dp[m][j] - 1;
+                    len = j - dp[m][j] + 1;
+                }
+            }
         }
-        return st == -1? "":S.substr(st, len);
+        return len == n + 1 ? "" : source.substr(start, len);
     }
 };
