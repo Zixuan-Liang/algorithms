33211a2650d21f6971f8c9ea76569a25e3d9f0a5
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-04 23:04:18 -0400

    google

--- C++/String/551. Student Attendance Record I.cpp
+++ C++/String/551. Student Attendance Record I.cpp
@@ -3,7 +3,7 @@ public:
     bool checkRecord(string s) {
         int late = 0;
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == 'L' && i < s.size() - 2 && s.substr(i, 3) == "LLL") {
+            if (s[i] == 'L' && s.substr(i, 3) == "LLL") {
                 return false;
             }
             else if (s[i] == 'A') {
