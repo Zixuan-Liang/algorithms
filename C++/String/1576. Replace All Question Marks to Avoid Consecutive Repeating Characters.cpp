0756ea1679c5143729e68c5f3c3b178ddf624e24
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-05 00:54:31 -0400

    google

--- C++/String/1576. Replace All Question Marks to Avoid Consecutive Repeating Characters.cpp
+++ C++/String/1576. Replace All Question Marks to Avoid Consecutive Repeating Characters.cpp
@@ -1,49 +1,13 @@
 class Solution {
 public:
+
     string modifyString(string s) {
-        string res;
-        int index = 0;
-        char left, right;
-        while (index < s.size()) {
-            if (s[index] != '?') {
-                res += s[index];
-                index++;
-            }
-            else {
-                if (index > 0) left = s[index - 1];
-                else left = '#';
-                int start = index;
-                while (index < s.size() && s[index] == '?') {
-                    index++;
-                }
-                int len = index - start;
-                if (index < s.size()) right = s[index];
-                else right = '#';
-                char fill1, fill2;
-                for (int i = 0; i < 26; i++) {
-                    if ('a' + i != left && 'a' + i != right) {
-                        fill1 = 'a' + i;
+        for (auto i = 0; i < s.size(); ++i)
+            if (s[i] == '?')
+                for (s[i] = 'a'; s[i] <= 'c'; ++s[i])
+                    if ((i == 0 || s[i - 1] != s[i]) && (i == s.size() - 1 || s[i + 1] != s[i]))
                         break;
-                    }
-                }
-                for (int i = 0; i < 26; i++) {
-                    if ('a' + i != left && 'a' + i != right && 'a' + i != fill1) {
-                        fill2 = 'a' + i;
-                        break;
-                    }
-                }
-                bool turn = true;
-                for (int i = 0; i < len; i++) {
-                    if (turn) {
-                        res += fill1;
-                    }
-                    else {
-                        res += fill2;
-                    }
-                    turn = !turn;
-                }
-            }
-        }
-        return res;
+        return s;
     }
+    
 };
