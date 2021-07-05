f0a4d37ef19056eaf81fd82b2c2f84d30537b9e9
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-05 01:01:24 -0400

    google

--- C++/Depth-First Search/529. Minesweeper.cpp
+++ C++/Depth-First Search/529. Minesweeper.cpp
@@ -1,23 +1,16 @@
 class Solution {
 
+    int m;
+    int n;
+    vector<pair<int, int>> adjs = {{-1,-1},{-1,1},{1,-1},{1,1},{-1,0},{1,0},{0,1},{0,-1}};
+    
 public:
     
-    vector<vector<int>> neighbors(int r, int c, int m, int n) {
-        vector<vector<int>> points = {{r-1,c-1},{r-1,c},{r-1,c+1},{r,c-1},{r,c+1},{r+1,c-1},{r+1,c},{r+1,c+1}};
-        vector<vector<int>> res;
-        for (auto& p : points) {
-            if (p[0] >= 0 && p[0] < m && p[1] >= 0 && p[1] < n) {
-                res.push_back(p);
-            }
-        }
-        return res;
-    }
-    
-    void helper(vector<vector<char>>& board, int r, int c, int m, int n) {
-        vector<vector<int>> points = neighbors(r, c, m, n);
+    void helper(vector<vector<char>>& board, int r, int c) {
         int count = 0;
-        for (auto& p : points) {
-            if (board[p[0]][p[1]] == 'M') {
+        for (auto [dr, dc] : adjs) {
+            int nr = r + dr, nc = c + dc;
+            if (nr>=0 && nr<m && nc>=0 && nc<n && board[nr][nc] == 'M') {
                 count++;
             }
         }
@@ -27,23 +20,25 @@ public:
         }
         else {
             board[r][c] = 'B';
-            for (auto& p : points) {
-                if (board[p[0]][p[1]] == 'E') {
-                    helper(board, p[0], p[1], m, n);
+            for (auto [dr, dc] : adjs) {
+                int nr = r + dr, nc = c + dc;
+                if (nr>=0 && nr<m && nc>=0 && nc<n && board[nr][nc] == 'E') {
+                    helper(board, nr, nc);
                 }
             }
         }
     }
     
     vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
-        int m = board.size(), n = board[0].size();
+        m = board.size();
+        n = board[0].size();
         int r = click[0], c = click[1];
         if (board[r][c] == 'M') {
             board[r][c] = 'X';
             return board;
         }
         else {
-            helper(board, r, c, m, n);
+            helper(board, r, c);
             return board;
         }
     }
