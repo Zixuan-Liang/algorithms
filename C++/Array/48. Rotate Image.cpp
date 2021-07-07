1af08b5b50eb449775dcbcc50d6c4360ef2e3088
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-07 17:13:03 -0400

    google

--- C++/Array/48. Rotate Image.cpp
+++ C++/Array/48. Rotate Image.cpp
@@ -16,7 +16,6 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        int temp1, temp2, temp3, temp4;
         for (int i = 0; i < n/2; i++) {
             for (int j = 0; j < (n+1)/2; j++) {
                 swap(matrix[i][j], matrix[j][n-1-i]);
