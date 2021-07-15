df6c6b1ca7d1cd769289deabc5e7774cc95e397a
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-15 09:31:19 -0400

    update

--- C++/Comments/Backtracking/489. Robot Room Cleaner.cpp
+++ C++/Comments/Backtracking/489. Robot Room Cleaner.cpp
@@ -21,23 +21,23 @@ public:
         robot.turnRight();
     }
     
-    void search(Robot& robot, int curr_x, int curr_y, int dir) {
+    void backtrack(Robot& robot, int curr_x, int curr_y, int dir) {
         visited.insert({curr_x, curr_y});
         robot.clean();
         for (int i = 0; i < 4; i++) {
             int new_dir = (dir+i) % 4;
             int new_x = curr_x + dirs[new_dir].first;
             int new_y = curr_y + dirs[new_dir].second;
-            if (!visited.count({new_x, new_y}) and robot.move()) {
-                search(robot, new_x, new_y, new_dir);
-                moveBack(robot);
+            if (!visited.count({new_x, new_y}) and robot.move()) { // 往前走
+                backtrack(robot, new_x, new_y, new_dir); // 回溯递归
+                moveBack(robot); // 取消往前走（保持方向）
             }
-            robot.turnRight();
+            robot.turnRight(); // 走到尽头后右转
         }
     }
     
     void cleanRoom(Robot& robot) {
-        search(robot, 0, 0, UP);
+        backtrack(robot, 0, 0, UP);
     }
 };
 
