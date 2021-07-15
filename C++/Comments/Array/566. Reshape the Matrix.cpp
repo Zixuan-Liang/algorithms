38312145460934a56136c9dd79680b13639bd1c2
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-14 20:40:20 -0400

    Update 566. Reshape the Matrix.cpp

--- C++/Comments/Array/566. Reshape the Matrix.cpp
+++ C++/Comments/Array/566. Reshape the Matrix.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        if (r * c != mat.size() * mat[0].size()) {
+        if (r * c != mat.size() * mat[0].size()) { // size doesn't match
             return mat;
         }
         else {
             int n = r * c, old_c = mat[0].size();
             vector<vector<int>> ans(r, vector<int>(c));
             for (int i = 0; i < n; i++) {
-                ans[i/c][i%c] = mat[i/old_c][i%old_c];
+                ans[i/c][i%c] = mat[i/old_c][i%old_c]; // transform 1d index to 2d index
             }
             return ans;
         }
