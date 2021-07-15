7e6851c8490b76ae589af52a1185cdb211551c68
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-15 06:06:24 -0400

    update

--- Python/22. Generate Parentheses.py
+++ Python/22. Generate Parentheses.py
@@ -1,14 +1,12 @@
-from typing import List
-
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
+    def generateParenthesis(self, n):
         res = []
-        def helper(s, open_, close_):
+        def backtrack(s, open_, close_):
             if len(s) == 2*n and open_ == n and close_ == n:
                 res.append(s)
             if open_+1 <= n:
-                helper(s+'(', open_+1, close_)
+                backtrack(s+'(', open_+1, close_)
             if close_+1 <= n and open_ > close_:
-                helper(s+')', open_, close_+1)
-        helper('', 0, 0)
+                backtrack(s+')', open_, close_+1)
+        backtrack('', 0, 0)
         return res
