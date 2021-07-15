bc0d605069dd99ef0c86ecdbb31f26ef49349da2
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-15 18:18:20 -0400

    update

--- C++/0.Comments/Binary Search/74. Search a 2D Matrix.cpp
+++ C++/0.Comments/Binary Search/74. Search a 2D Matrix.cpp
@@ -4,13 +4,13 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>> matrix, int target) {
         // upper_bound返回比target大的第一个元素
-        auto row = upper_bound(matrix.begin(), matrix.end(), target, [](const int b, const vector<int> &a) {
+        auto row = upper_bound(matrix.begin(), matrix.end(), target, [](const int b, const vector<int> &a) { // 先找出元素在哪一行
             return b < a[0];
         });
         if (row == matrix.begin()) {
             return false;
         }
         --row;
-        return binary_search(row->begin(), row->end(), target);
+        return binary_search(row->begin(), row->end(), target); // 在对应行查找元素
     }
 };
--- C++/0.Comments/Binary Search/Write Binary Search Correctly.cpp
+++ C++/0.Comments/Binary Search/Write Binary Search Correctly.cpp
@@ -1,3 +1,9 @@
+// Important APIs:
+// lower_bound(begin, end, target) ->iterator 第一个大于等于目标
+// upper_bound(begin, end, target) ->iterator 第一个大于目标
+// binary_search(begin, end, target) ->bool (<algorithm>)
+// euqual_range(begin, end, targetRange, compareFunc) -> {firstEqualIt, lastEqualIt+1} (<algorithm>) 定义比较函数区间1<区间2：区间1的终点严格小于区间2的起点，则equal_range查找所有和target区间相交的区间
+
 // Implementation 1
 // A[low] <= A[i] <  A[high]
 int binarySearch(vector<int> A, int x) {
