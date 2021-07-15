95403f4b0cdd22f4835d800f749fb7fcdfd2e07e
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-15 19:11:59 -0400

    update

--- C++/0.Comments/Binary Search/222. Count Complete Tree Nodes.cpp
+++ C++/0.Comments/Binary Search/222. Count Complete Tree Nodes.cpp
@@ -22,37 +22,37 @@ public:
     }
     
     bool exists(int idx, int d, TreeNode* root) {
-        int left = 0, right = pow(2, d) - 1;
+        int left = 0, right = pow(2, d) - 1; // 循环不变量，答案可能存在于[left, right]
         for (int i = 0; i < d; i++) {
             int pivot = (left + right) / 2;
-            if (idx <= pivot) {
+            if (idx <= pivot) { // 往左查找，保持循环不变量：答案可能存在于[left, right]
                 root = root->left;
                 right = pivot;
             }
-            else {
+            else { // 往右查找，保持循环不变量：答案可能存在于[left, right]
                 root = root->right;
                 left = pivot + 1;
             }
         }
-        return root != nullptr;
+        return root != nullptr; // 检查答案是否存在
     }
     
     int countNodes(TreeNode* root) {
         if (!root) return 0;
         int d = computeDepth(root);
         if (d == 0) return 1;
-        
-        int left = 1, right = pow(2, d) - 1;
-        while (left <= right) {
-            int pivot = (left + right) / 2;
-            if (exists(pivot, d, root)) {
+        // Y代表存在，N代表不存在
+        // YYY...YYYNNN... 查找第一个不存在的index
+        int left = 1, right = pow(2, d);
+        while (left < right) { // 循环不变量，答案必存在于[left, right]
+            int pivot = left + (right - left) / 2;
+            if (exists(pivot, d, root)) { // 保持循环不变量：答案必存在于[left, right]
                 left = pivot + 1;
             }
             else {
-                right = pivot - 1;
+                right = pivot;
             }
         }
-        
-        return pow(2, d) - 1 + left;
+        return pow(2, d) - 1 + left; // 循环打破，left=right，left就是第一个不存在的index，即为最后一层的节点数
     }
 };
