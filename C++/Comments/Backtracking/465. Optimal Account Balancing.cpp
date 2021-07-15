b87fb10cef5cbae94254cf375a1aaae756e4e346
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-15 09:27:01 -0400

    update

--- C++/Comments/Backtracking/465. Optimal Account Balancing.cpp
+++ C++/Comments/Backtracking/465. Optimal Account Balancing.cpp
@@ -16,14 +16,14 @@ public:
     
     int backtrack(int start) {
         int res = INT_MAX;
-        while (start < debts.size() && debts[start] == 0) start++;
+        while (start < debts.size() && debts[start] == 0) start++; // 跳过收支平衡的（0）
         for (int i = start + 1; i < debts.size(); i++) {
-            if (debts[i] * debts[start] < 0) {
+            if (debts[i] * debts[start] < 0) { // 往后找出与当前不一样的进行回溯，当前为正的找负，当前为负的找正，然后直接把当前的债务全部转移给后面的
                 debts[i] += debts[start];
-                res = min(res, 1 + backtrack(start + 1));
+                res = min(res, 1 + backtrack(start + 1)); // 更新答案
                 debts[i] -= debts[start];
             }
         }
-        return res == INT_MAX ? 0 : res;
+        return res == INT_MAX ? 0 : res; // 如果答案没更新过，说明不需要进行任何操作
     }
 };
