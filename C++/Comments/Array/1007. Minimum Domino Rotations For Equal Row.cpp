aeccbefbd8511dc579ffe67bb1b722a70cb1f040
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-14 21:24:47 -0400

    update

--- C++/Comments/Array/1007. Minimum Domino Rotations For Equal Row.cpp
+++ C++/Comments/Array/1007. Minimum Domino Rotations For Equal Row.cpp
@@ -4,16 +4,16 @@ public:
         vector<vector<int>> valCount(7, vector<int>(3, 0));
         for (int i = 0; i < tops.size(); i++) {
             if (tops[i] == bottoms[i]) {
-                valCount[tops[i]][2]++;
+                valCount[tops[i]][2]++; // 共有的
             }
             else {
-                valCount[tops[i]][0]++;
-                valCount[bottoms[i]][1]++;
+                valCount[tops[i]][0]++; // top独有的
+                valCount[bottoms[i]][1]++; // bottom独有的
             }
         }
         int res = INT_MAX;
         for (int i = 1; i <= 6; i++) {
-            if (valCount[i][0] + valCount[i][1] + valCount[i][2] == tops.size()) {
+            if (valCount[i][0] + valCount[i][1] + valCount[i][2] == tops.size()) { // 可翻转成功的
                 res = min(res, min(valCount[i][0], valCount[i][1]));
             }
         }
--- C++/Comments/Array/1526. Minimum Number of Increments on Subarrays to Form a Target Array.cpp
+++ C++/Comments/Array/1526. Minimum Number of Increments on Subarrays to Form a Target Array.cpp
@@ -3,7 +3,7 @@ public:
     int minNumberOperations(vector<int>& A) {
         int res = 0, pre = 0;
         for (int a: A) {
-            res += max(a - pre, 0);
+            res += max(a - pre, 0); // 当前数值比前一个数超出x就需要添加x次递增
             pre = a;
         }
         return res;
