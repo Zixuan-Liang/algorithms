2f55fe37bbdbbd704ce6c8ae6cf81515796714f7
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-18 06:11:05 -0400

    update

--- C++/Dynamic Programming/279. Perfect Squares.cpp
+++ C++/Dynamic Programming/279. Perfect Squares.cpp
@@ -23,20 +23,3 @@ public:
         return helper(n, memo);
     }
 };
-
-// class Solution {
-// public:
-//     int numSquares(int n) {
-//         vector<int> perfSq(n+1,INT_MAX);
-//         perfSq[0]=0;
-//         for(int i=1;i<=n;i++)
-//         {
-//             for(int j=1;j*j<=i;j++)
-//             {
-//                 perfSq[i]= min(perfSq[i],perfSq[i-j*j]+1);
-//             }
-//         }
-//         return perfSq[n];
-        
-//     }
-// };
