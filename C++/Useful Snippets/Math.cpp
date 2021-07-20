f289ac180d18544e9876320d171ee1eb05971431
Zixuan-Liang <zixuan.liang@outlook.com>
2021-07-19 23:17:49 -0400

    update

--- C++/Useful Snippets/Math.cpp
+++ C++/Useful Snippets/Math.cpp
@@ -8,3 +8,21 @@ int gcd(int a, int b) {
     if (b == 0) return a;
     return gcd(b, a%b);
 }
+
+// 判断质数
+// 质数有一个特点，就是它总是等于 6x-1 或者 6x+1，其中 x 是大于等于1的自然数。
+// 所以循环的步长可以设为 6，然后每次只判断两侧的数即可。
+bool isPrime(int x) {
+    if (x <= 3) {
+        return x > 1;
+    }
+    if (x % 6 != 1 && num % 6 != 5) {
+        return false;
+    }
+    for (int i = 5; i * i <= x; i += 6) {
+        if (x % i == 0 || x % (i + 2) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
